f19090f2875456620449205d442644e220e85436
Spicciani <marco.spicciani@gmail.com>
2022-11-14 16:19:04 +0100

    Initial Commit

---
+++ PrjPhoto/PrjPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrjPhotoApp.swift
+//  PrjPhoto
+//
+//  Created by MacMini on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PrjPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
